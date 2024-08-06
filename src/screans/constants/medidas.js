245275445841231c4dd4016cbe52e3fee7e56d80
ricgcs <rg530755@gmail.com>
2024-08-06 12:45:45 -0300

    first commit

---
+++ src/screans/constants/medidas.js
@@ -0,0 +1,7 @@
+import { Dimensions } from "react-native";
+// pasata contendo dados que eerão reutilizados na página inteira
+
+export const width = Dimensions.get("window").width;
+export const height = Dimensions.get("window").height;
+
+
---
+++ src/screans/screans/Main.jsx
@@ -0,0 +1,29 @@
+//jsx retorna uma interface, ou seja não contem logica
+// default retorna um objeto
+
+import { View, Text } from "react-native";
+
+// o nome da função precisa ser o mesmo do arquivo e letra maiuscula
+export default function Main(){
+return(
+    <View>
+    <View>
+    <Text></Text>
+    <Text></Text>
+
+    </View>
+    <Touchableopacity>
+    </Touchableopacity>
+    </View>
+)
+}
+
+
+const styles = StyleSheet.create({
+topArea:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center",
+
+}
+})
